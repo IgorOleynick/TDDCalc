58df0c6c46bda52bbb752215e5590631a86b9bf0
Igor Oleynick <originalname@inbox.ru>
2014-04-25 18:09:05 +0600

    Создал проект и написал тест для сложения

---
+++ BusinessLogic/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Calculator
+    {
+        public static int Add()
+        {
+            return 4;
+        }
+    }
+}
---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using BusinessLogic;
+
+namespace Tests
+{
+    public class CalculatorTests
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Fact]
+        public void Add()
+        {
+            var calculator = new Calculator();
+            int result = Calculator.Add();
+            Assert.Equal(4, result);
+        }
+    }
+}
